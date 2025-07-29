c75f71c08823f80283ba3d09bdd84bf3979425ba
malathair <malathair@spectregate.net>
2025-07-29 11:01:53 -0600

    Fix alternate user handling for jumphosting

--- cmd/ssm.go
+++ cmd/ssm.go
@@ -14,14 +14,31 @@ import (
 	"github.com/spf13/viper"
 )
 
-func buildFqdn(host string) string {
+func buildFqdn(userAndHost string) (string, string) {
+	var (
+		host string
+		user string
+	)
+
+	// Handle splitting user and host if an alternate username is supplied
+	// Users are expected with the standard <user>@<host> format
+	parts := strings.Split(userAndHost, "@")
+	if len(parts) > 2 {
+		log.Fatalf("Invalid user and host combination: %s", userAndHost)
+	} else if len(parts) == 2 {
+		user = parts[0]
+		host = parts[1]
+	} else {
+		host = parts[0]
+	}
+
 	ip := net.ParseIP(host)
 	if ip != nil {
 		if config.dryRun {
 			fmt.Printf("%s is valid IP address\n", host)
 			fmt.Println()
 		}
-		return ip.String()
+		return user, ip.String()
 	} else if config.dryRun {
 		fmt.Printf("%s is not a valid IP Address\n", host)
 	}
@@ -32,14 +49,14 @@ func buildFqdn(host string) string {
 			fmt.Printf("%s is a valid FQDN\n", host)
 			fmt.Println()
 		}
-		return host
+		return user, host
 	} else if config.dryRun {
 		fmt.Printf("%s is not a valid FQDN (%s)\n", host, err)
 	}
 
 	if len(config.domains) < 1 {
 		fmt.Println("No domains found for FQDN autocompletion")
-		return ""
+		return user, ""
 	}
 
 	if config.dryRun {
@@ -53,13 +70,13 @@ func buildFqdn(host string) string {
 				fmt.Printf("%s is a valid FQDN\n", fqdn)
 				fmt.Println()
 			}
-			return fqdn
+			return user, fqdn
 		} else if config.dryRun {
 			fmt.Printf("%s is not a valid FQDN (%s)\n", fqdn, err)
 		}
 	}
 
-	return ""
+	return user, ""
 }
 
 func buildSshArgs(user, fqdn string) ([]string, error) {
@@ -83,11 +100,15 @@ func buildSshArgs(user, fqdn string) ([]string, error) {
 	//  when jumphosting. Either need to disable the use of sshpass when jumphosting or implement
 	//  a version of it that can handle multiple password prompts in succession
 	if config.jump || config.jumphost != viper.GetString("flags.jumphost") {
-		jumphost := buildFqdn(config.jumphost)
+		jumpuser, jumphost := buildFqdn(config.jumphost)
 		if jumphost == "" {
 			return nil, errors.New(fmt.Sprintf("Failed to find valid FQDN for jumphost %s\n", config.jumphost))
 		}
-		sshArgs = append(sshArgs, "-J", jumphost)
+		if jumpuser != "" {
+			sshArgs = append(sshArgs, "-J", jumpuser+"@"+jumphost)
+		} else {
+			sshArgs = append(sshArgs, "-J", jumphost)
+		}
 
 	}
 
@@ -102,31 +123,16 @@ func buildSshArgs(user, fqdn string) ([]string, error) {
 	return sshArgs, nil
 }
 
-func ssm(userAndHost string) error {
+func ssm(host string) error {
 	var (
 		err      error
 		fqdn     string
-		host     string
-		parts    []string
 		shellCmd *exec.Cmd
 		sshArgs  []string
 		user     string
 	)
 
-	// Handle splitting user and host if an alternate username is supplied
-	// Users are expected with the standard <user>@<host> format
-	parts = strings.Split(userAndHost, "@")
-	if len(parts) > 2 {
-		log.Fatalf("Invalid user and host combination: %s", userAndHost)
-	} else if len(parts) == 2 {
-		user = parts[0]
-		host = parts[1]
-	} else {
-		user = ""
-		host = parts[0]
-	}
-
-	fqdn = buildFqdn(host)
+	user, fqdn = buildFqdn(host)
 	if fqdn == "" {
 		return errors.New(fmt.Sprintf("Failed to find valid FQDN for %s", host))
 	}
